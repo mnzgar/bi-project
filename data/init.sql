89a2c01706105828891597f4074feeed72635fd0
Míriam Núñez García <mirinu97@gmail.com>
2024-12-21 18:04:38 +0000

    :bug: Add provinces table

--- data/init.sql
+++ data/init.sql
@@ -19,3 +19,15 @@ COPY countries(code, name)
 FROM '/docker-entrypoint-initdb.d/codes/pais.csv'
 DELIMITER ','
 CSV HEADER;
+
+-- Crear tabla para provincias
+CREATE TABLE provinces (
+    code CHAR(2) PRIMARY KEY,
+    prov_name TEXT NOT NULL,
+    caut_name TEXT NOT NULL
+);
+
+COPY provinces(code, prov_name, caut_name)
+FROM '/docker-entrypoint-initdb.d/codes/provincia.csv'
+DELIMITER ','
+CSV HEADER;
