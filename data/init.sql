8cf346bacd5c72061644d4503cdbcf2bc6886513
Míriam Núñez García <mirinu97@gmail.com>
2025-01-05 19:34:30 +0000

    :sparkles: Add new data container

--- data/init.sql
+++ data/init.sql
@@ -1,33 +1,12 @@
--- Crear tabla para los modos de transporte
-CREATE TABLE transport_modes (
-    code INT PRIMARY KEY,
-    name TEXT NOT NULL
+CREATE TABLE importacion(
+    ano CHAR(2),
+    mes CHAR(2),
+    fecha_admision_documento DATE,
+    producto VARCHAR(100),
+    pais_origen VARCHAR(100),
+    provincia_destino VARCHAR(50),
+    peso BIGINT,
+    valor_factura BIGINT,
+    modo_transporte_frontera VARCHAR(255),
+    zona_intercambio VARCHAR(50)
 );
-
-COPY transport_modes(code, name)
-FROM '/docker-entrypoint-initdb.d/codes/modo_transporte.csv'
-DELIMITER ','
-CSV HEADER;
-
--- Crear tabla para los países
-CREATE TABLE countries (
-    code CHAR(2) PRIMARY KEY,
-    name TEXT NOT NULL
-);
-
-COPY countries(code, name)
-FROM '/docker-entrypoint-initdb.d/codes/pais.csv'
-DELIMITER ','
-CSV HEADER;
-
--- Crear tabla para provincias
-CREATE TABLE provinces (
-    code CHAR(2) PRIMARY KEY,
-    prov_name TEXT NOT NULL,
-    caut_name TEXT NOT NULL
-);
-
-COPY provinces(code, prov_name, caut_name)
-FROM '/docker-entrypoint-initdb.d/codes/provincia.csv'
-DELIMITER ','
-CSV HEADER;
